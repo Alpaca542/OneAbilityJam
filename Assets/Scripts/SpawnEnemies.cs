f67550e3f7a28e39c52c7bd9cd983fa5bf370bb2
Alpaca <mihaililyin08@gmail.com>
2024-06-08 16:38:33 +0300

    stuff

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public float startingValue = 1.5f;
+    private float currentValue;
+    public float Enemyamount;
+    public GameObject[] Enemies;
+    public Transform anchor1;
+    public Transform anchor2;
+
+    void Start()
+    {
+        currentValue = startingValue;
+        StartCoroutine(SpawningCourutine());
+    }
+
+    public IEnumerator SpawningCourutine()
+    {
+        for (int i = 0; i < Enemyamount; i++)
+        {
+            yield return new WaitForSeconds(currentValue);
+            Instantiate(Enemies[Random.Range(0, Enemies.Length)], new Vector2(Random.Range(anchor1.position.x, anchor2.position.x), Random.Range(anchor1.position.y, anchor2.position.y)), Quaternion.identity);
+            currentValue -= 0.01f;
+        }
+    }
+}
