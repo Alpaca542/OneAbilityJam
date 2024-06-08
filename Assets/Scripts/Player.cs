b685e8f39b789d51ce0752f216cd0bcac6534e13
Alpaca <mihaililyin08@gmail.com>
2024-06-08 13:23:14 +0300

    WhateverImTired

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,6 @@ public class Player : MonoBehaviour
 
     [Header("Debug")]
     public LayerMask WhatToCheckOnJump;
-    public bool AmIFlying = false;
     private Rigidbody2D rb;
     public Transform rayer1;
     public Transform rayer2;
@@ -26,31 +25,24 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        AmIFlying = false;
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        AmIFlying = true;
-    }
     void Update()
     {
-
-        if (!AmIFlying)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal != 0)
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            if (moveHorizontal != 0)
+            if (moveHorizontal > 0.2 && transform.rotation != Quaternion.Euler(0, 0, 0))
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            else if (moveHorizontal < -0.2 && transform.rotation != Quaternion.Euler(0, -180, 0) && transform.rotation != Quaternion.Euler(0, 180, 0))
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+
+        if(Physics2D.Raycast(rayer1.position, Vector2.down, 0.001f, WhatToCheckOnJump) || Physics2D.Raycast(rayer2.position, Vector2.down, 0.001f, WhatToCheckOnJump) || Physics2D.Raycast(rayer3.position, Vector2.down, 0.001f, WhatToCheckOnJump))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (moveHorizontal > 0.2 && transform.rotation != Quaternion.Euler(0, 0, 0))
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                else if (moveHorizontal < -0.2 && transform.rotation != Quaternion.Euler(0, -180, 0) && transform.rotation != Quaternion.Euler(0, 180, 0))
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
+                rb.AddForce(Vector2.up * jumpForce);
             }
-
-            rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
-
             if (rb.velocity == Vector2.zero)
             {
                 myAnimator.SetBool("Walking", false);
@@ -60,15 +52,6 @@ public class Player : MonoBehaviour
             {
                 myAnimator.SetBool("Walking", true);
                 myAnimator.SetBool("Jumping", false);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space) && (Physics2D.Raycast(rayer1.position, Vector2.down, 0.001f, WhatToCheckOnJump) || Physics2D.Raycast(rayer2.position, Vector2.down, 0.001f, WhatToCheckOnJump) || Physics2D.Raycast(rayer3.position, Vector2.down, 0.001f, WhatToCheckOnJump)))
-            {
-                rb.AddForce(Vector2.up * jumpForce);
-                AmIFlying = true;
-                myAnimator.SetBool("Walking", false);
-                myAnimator.SetBool("Jumping", true);
-
             }
         }
         else
