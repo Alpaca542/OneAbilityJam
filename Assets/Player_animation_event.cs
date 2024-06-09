cc60425921e3bb11282ed767b98e1bf703edf750
BunySenpai <Daviddoan2003@gmail.com>
2024-06-09 12:01:17 -0700

    adding events

---
+++ Assets/Player_animation_event.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_animation_event : MonoBehaviour
+{
+    public
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Player_Jump()
+    {
+        
+    }
+}
